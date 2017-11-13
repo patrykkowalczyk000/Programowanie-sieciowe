1a2355dc6b62c5b3cbf238ff1869f425bb536c26
Patryk <patryk.kowalczyk000@gmail.com>
2017-11-13 19:21:57 +0100

    Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        private static byte[] dane = new byte[10] {1, 1, 0, 1, 1, 1, 1, 1, 1, 1 };
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
